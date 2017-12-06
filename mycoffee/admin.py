e99ad39075e2956faf655f3c4f8a25d16a45a95d
Aziz <aalzamel@gmail.com>
2017-12-06 20:39:23 +0300

    admin page setup

--- mycoffee/admin.py
+++ mycoffee/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Bean, Roast, Syrups, Powders, Coffee
+
+admin.site.register(Bean)
+admin.site.register(Roast)
+admin.site.register(Syrups)
+admin.site.register(Powders)
+admin.site.register(Coffee)
